cef8869763f46cc565305c061735f8ba70cb642c
finisher <ksksysy540@gmail.com>
2026-03-27 09:32:12 +0530

    Deleted duplicate youtube-factory folder 🧹

--- youtube-factory/agents/__init__.py
+++
@@ -1,6 +0,0 @@
-from .director_agent import run_director
-from .analytics_agent import fetch_channel_analytics
-from .research_agent import get_trending_topic
-from .script_agent import generate_script, build_timeline, get_full_script_text
-from .production_agent import generate_audio, render_video
-from .upload_agent import upload_video
--- youtube-factory/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .llm_client import LLMClient
-from .asset_manager import fetch_icon
-from .sheets_client import log_video, update_status, ensure_headers
